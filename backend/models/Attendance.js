b7a6c54a3ac2e84465052c38e2f684971286fab2
chandanaMellachervu <161910287+chandanaMellachervu@users.noreply.github.com>
2025-03-28 13:27:30 +0530

    Delete backend directory

--- backend/models/Attendance.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const attendanceSchema = new mongoose.Schema({
-    date: { type: String, required: true },
-    attendance: { type: Object, required: true } // { "studentRegdNo": true/false }
-});
-
-const Attendance = mongoose.model("Attendance", attendanceSchema);
-module.exports = Attendance;
--- backend/models/Review.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const reviewSchema = new mongoose.Schema({
-  batchNumber: { type: String, required: true, unique: true },
-  reviews: { type: Object, required: true }
-});
-
-const Review = mongoose.model("Review", reviewSchema);
-module.exports = Review;
